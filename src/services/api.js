d4d6bf60c63cbf486754e200fd78239dc6dbe8ad
HOUNGUE110 <adigedeon6@gmail.com>
2026-07-07 16:42:20 +0000

    Fix: connect to production backend API

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuration de l'URL de base de ton API Laravel
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://gemylaravel.onrender.com',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
