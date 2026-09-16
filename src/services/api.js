4c452506037ecc5ab73de37ac8adf02ef0c66c98
HOUNGUE110 <adigedeon6@gmail.com>
2026-09-16 07:49:52 +0000

    Fix local storage keys and API payload alignment

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,15 @@
 import axios from 'axios';
 
-// Configuration de l'URL de base de ton API Laravel
 const API = axios.create({
-    baseURL: 'https://gemylaravel.onrender.com',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+  baseURL: 'https://gemylaravel.onrender.com',
 });
 
-// Middleware pour injecter automatiquement le Token s'il existe
 API.interceptors.request.use((config) => {
-    const token = localStorage.getItem('gemy_token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
+  const token = localStorage.getItem('gemy_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default API;
